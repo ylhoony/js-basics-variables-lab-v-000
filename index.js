3b2a63a95e656404ffae56a918834402002994d2
Hoon Lee <yl.hoony@gmail.com>
2017-09-28 04:45:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 
+const mostProfitableNeighborhood = 'Chelsea';
 
 
-// describe('companyName', function () {
-//   it('is set as Scuber', function () {
-//     expect(companyName).to.equal('Scuber');
-//   });
-//
-//   it('raises error if the companyName is changed', function () {
-//     expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
-//   });
-// });
 //
 // describe('mostProfitableNeighborhood', function () {
 //   it('is declared as equal to Chelsea', function () {
