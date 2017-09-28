edf9d4c6f8907a25231a667ce2412ba70e000e41
Hoon Lee <yl.hoony@gmail.com>
2017-09-28 04:47:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,3 @@ const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 
 let companyCeo = 'Susan Smith';
-
-//
-// describe('companyCeo', function () {
-//   it('is declared as equal to Susan Smith', function () {
-//     expect(companyCeo).to.equal('Susan Smith');
-//   });
-//
-//   it('does not raise error if the companyCeo is changed', function () {
-//     expect(function () { companyCeo = 'Lauren Hart' }).to.not.throw(TypeError);
-//   });
-// });
